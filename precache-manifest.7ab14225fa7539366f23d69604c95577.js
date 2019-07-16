a94b880756f1c581753d0329195cd6db02260d82
zonekid <marcusmiller220@gmail.com>
2019-07-16 18:17:50 -0400

    Updates

---
+++ precache-manifest.7ab14225fa7539366f23d69604c95577.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4661f280720fc612f0d0cbd05999eda3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b58d888e80e335a46cd",
+    "url": "/static/css/main.9a4fb390.chunk.css"
+  },
+  {
+    "revision": "b2ec133881986f7d771f",
+    "url": "/static/js/2.3fdd0728.chunk.js"
+  },
+  {
+    "revision": "0b58d888e80e335a46cd",
+    "url": "/static/js/main.b7e9420b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e42b7af835da5b5e2024fc282b4c4ec7",
+    "url": "/static/media/Hollywood.e42b7af8.jpg"
+  },
+  {
+    "revision": "444409a9f45196cc81696c0548d273c5",
+    "url": "/static/media/John Russel Hands-min2.444409a9.jpg"
+  },
+  {
+    "revision": "18d44f79b3979ec168862093208c6d7d",
+    "url": "/static/media/RobotoRegular.18d44f79.ttf"
+  },
+  {
+    "revision": "d1bf5f0043c8cdc7b9bfd5931feb39d6",
+    "url": "/static/media/SITW_Hov_P.d1bf5f00.jpg"
+  },
+  {
+    "revision": "e2f73f8e80f237cfd38b52bb1fe02711",
+    "url": "/static/media/SITW_multiple2.e2f73f8e.jpg"
+  },
+  {
+    "revision": "d681f4ec44a52a3dc72d0657ad217a16",
+    "url": "/static/media/SITW_purp_green_float.d681f4ec.jpeg"
+  },
+  {
+    "revision": "f34d07152a2efaa897e3d6b6217fea1a",
+    "url": "/static/media/Supreme.f34d0715.jpg"
+  },
+  {
+    "revision": "538a2161f9af76043ddfd7f5c6d61f7e",
+    "url": "/static/media/gotbackdrop2.538a2161.jpg"
+  },
+  {
+    "revision": "cc9d0df7561bfaefbcc3e2721de47b2d",
+    "url": "/static/media/marcusdraw.cc9d0df7.png"
+  }
+]);
